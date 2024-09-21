d1c3ddba32b5d134afc17aaedee63b4e86444905
MiyT645 <167467641+Sapphire645@users.noreply.github.com>
2024-09-21 13:15:53 +0200

    Alpha1

---
+++ control/ComboBoxArmour.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+
+namespace DQB2NPCViewer.control
+{
+    /// <summary>
+    /// Interaction logic for ComboBoxModel.xaml
+    /// </summary>
+    public partial class ComboBoxArmour : UserControl
+    {
+        public ushort ID { get; set; } = 0;
+        public Equipment Armour { get; set; }
+        public string Image { get; set; }
+        public Color Colour { get; set; }
+        public ComboBoxArmour()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public void SetImage()
+        {
+            this.ImageCalc.Source = new BitmapImage(new Uri(Image, UriKind.RelativeOrAbsolute));
+            RectangleCol.Fill = new SolidColorBrush(Colour);
+        }
+    }
+}
---
+++ control/Hearts.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace DQB2NPCViewer.control
+{
+    /// <summary>
+    /// Interaction logic for Hearts.xaml
+    /// </summary>
+    public partial class Hearts : UserControl
+    {
+        private ushort ID { get; set; } = 0;
+        public Hearts()
+        {
+            InitializeComponent();
+        }
+
+        public void HeartsCommand(ushort i, string type)
+        {
+            ushort ie;
+            ID = i;
+            TextBlockHeart.Text = ID.ToString() + " - ";
+            for (ie = 0; ie < ID; ie++) //ID
+            {
+                StackPanelHearts.Children.Add(new Image
+                {
+                    Source = new BitmapImage(new Uri("/images/resource/" + type + "Heart.png", UriKind.RelativeOrAbsolute)),
+                    Height = 15,
+                    Width = 15,
+                    Margin = new Thickness(2, 0, 2, 0)
+                });
+            }
+            for (var a = ie; a < 5; a++) //ID
+            {
+                StackPanelHearts.Children.Add(new Image
+                {
+                    Source = new BitmapImage(new Uri("/images/resource/Heart.png", UriKind.RelativeOrAbsolute)),
+                    Height = 15,
+                    Width = 15,
+                    Margin = new Thickness(2, 0, 2, 0)
+                });
+            }
+        }
+    }
+}
