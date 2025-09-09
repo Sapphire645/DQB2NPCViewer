cbef2b9e825c9c53a2eaf28842a479c78c955e22
MiyT645 <167467641+Sapphire645@users.noreply.github.com>
2025-09-09 16:25:44 +0200

    I think it should compile now

--- code/BuilderData.cs
+++ code/BuilderData.cs
@@ -89,12 +89,12 @@ namespace DQB2NPCViewer.code
             {
                 var NameBytes = new byte[12];
                 Array.Copy(byteDataName, 0, NameBytes, 0, 12);
-                return System.Text.UTF8Encoding.GetString(NameBytes);
+                return System.Text.UTF8Encoding.Default.GetString(NameBytes);
             }
             set
             {
                 var NameBytes = new byte[12];
-                NameBytes = System.Text.UTF8Encoding.GetBytes(value);
+                NameBytes = System.Text.UTF8Encoding.Default.GetBytes(value);
                 Array.Copy(NameBytes, 0, byteDataName, 0, 12);
             }
         }
